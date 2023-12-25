378741194ed7d64608ead47b36d94a322d9f8cd9
Bismuth10K <bismuthdev@gmail.com>
2023-12-25 19:49:19 +0100

    dunno

--- honk_code/src/main/java/module-info.java
+++ honk_code/src/main/java/module-info.java
@@ -1,16 +1,16 @@
 module honk.honk_code {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.web;
-	
-	requires org.controlsfx.controls;
-	requires com.dlsc.formsfx;
-	requires net.synedra.validatorfx;
-	requires org.kordamp.ikonli.javafx;
-	requires org.kordamp.bootstrapfx.core;
-	requires eu.hansolo.tilesfx;
-	requires com.almasb.fxgl.all;
-	
-	exports honk.honk_code;
-	opens honk.honk_code to javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens honk.honk_code to javafx.fxml;
+    exports honk.honk_code;
 }
