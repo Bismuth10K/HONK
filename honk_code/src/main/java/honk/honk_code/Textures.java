e37006e9ba9b9f48cbeb0257a6b49c4f0a309998
Bismuth10K <bismuthdev@gmail.com>
2023-12-31 15:51:07 +0100

    Chaque tamagotchi a un sprite

--- honk_code/src/main/java/honk/honk_code/Textures.java
+++ honk_code/src/main/java/honk/honk_code/Textures.java
@@ -20,10 +20,10 @@ public class Textures {
 	public static final Image WalkActionButtonSpr = new Image(texturesDir + "gauges-and-buttons/WalkButtons.png");
 	public static final Image WashActionButtonSpr = new Image(texturesDir + "gauges-and-buttons/WashButton.png");
 	//Animals Full body
-	public static final Image CatTamaSpr = new Image(texturesDir + "animals/CatFull.png");
-	public static final Image DogTamaSpr = new Image(texturesDir + "animals/DogFull.png");
-	public static final Image RabbitTamaSpr = new Image(texturesDir + "animals/RabbitFull.png");
+	public static final Image CatTamaSpr = new Image(String.valueOf(Textures.class.getResource(texturesDir + "animals/CatFull.png")));
+	public static final Image DogTamaSpr = new Image(String.valueOf(Textures.class.getResource(texturesDir + "animals/DogFull.png")));
+	public static final Image RabbitTamaSpr = new Image(String.valueOf(Textures.class.getResource(texturesDir + "animals/RabbitFull.png")));
+	public static final Image RobotTamaSpr = new Image(String.valueOf(Textures.class.getResource(texturesDir + "animals/RobotFull.png")));
 	
 	//Options Buttons
-	public static final Image RobotTamaSpr = new Image(texturesDir + "animals/RobotFull.png");
 }
